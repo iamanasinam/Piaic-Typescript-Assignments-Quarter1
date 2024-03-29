4567a9b5eacf7c2aecd4916375c12be199faf8d4
Anas <iamanasinam@gmail.com>
2024-03-29 12:04:33 +0500

    Q2, commented finding area of rectangle completed

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 // Calculate your age based on the current year and your birth year.
-let brithYear: number=2000;
-let yearNow: number=2024;
+// let brithYear: number=2000;
+// let yearNow: number=2024;
 
-console.log(`Q1\nI'm ${yearNow - brithYear} years old.`)
+// console.log(`Q1\nI'm ${yearNow - brithYear} years old.`)
 
 // Write a program that calculates the area of a rectangle using length and width variables
-let _length: number=20;
-let _width: number=24;
+// let _length: number=20;
+// let _width: number=24;
 
-console.log(`\nQ2\nLength = ${_length}, Width = ${_width}\nArea of Triangle = ${_length * _width}`)
+// console.log(`\nQ2\nLength = ${_length}, Width = ${_width}\nArea of Triangle = ${_length * _width}`)
