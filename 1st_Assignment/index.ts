627520a59fd10e922e716a3659abd53c6c12b858
Anas <iamanasinam@gmail.com>
2024-03-30 01:58:41 +0500

    adding proper variable in the code

--- 1st_Assignment/index.ts
+++ 1st_Assignment/index.ts
@@ -1,7 +1,8 @@
 // Q1: Calculate your age based on the current year and your birth year.
 let brithYear: number=2000;
 let yearNow: number=2024;
-console.log(`Q1: Finding age based on the current year.\nI'm ${yearNow - brithYear} years old.`)
+let age :number = yearNow - brithYear;
+console.log(`Q1: Finding age based on the current year.\nI'm ${age} years old.`)
 
 // Q2:Write a program that calculates the area of a rectangle using length and width variables
 let _length: number=20;
