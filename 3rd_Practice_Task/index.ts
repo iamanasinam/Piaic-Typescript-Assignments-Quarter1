85d498f6793db386a7aae6390fe82acf4f65b228
Anas <iamanasinam@gmail.com>
2024-04-03 10:19:03 +0500

    4th task has been added

--- 3rd_Practice_Task/index.ts
+++ 3rd_Practice_Task/index.ts
@@ -49,16 +49,36 @@
 
 // - Write a program that takes the age of a person as input and determines whether they are a child (0-12 years), teenager (13-19 years), adult (20-59 years), or senior citizen (60 years and above)
 
-let age = 20;
+// let age = 20;
 
-if (age >= 0 && age <= 12) {
-    console.log(`\nChild: ${age}`);
-} else if (age >= 13 && age <= 19) {
-    console.log(`\nTeenager: ${age}`);
-} else if (age >= 20 && age <= 59) {
-    console.log(`\nAdult: ${age}`);
-} else if (age >= 60) {
-    console.log(`\nSenior Citizen: ${age}`);
+// if (age >= 0 && age <= 12) {
+//     console.log(`\nChild: ${age}`);
+// } else if (age >= 13 && age <= 19) {
+//     console.log(`\nTeenager: ${age}`);
+// } else if (age >= 20 && age <= 59) {
+//     console.log(`\nAdult: ${age}`);
+// } else if (age >= 60) {
+//     console.log(`\nSenior Citizen: ${age}`);
+// } else {
+//     console.log(`\n\nInvalid Age: ${age}`);
+// }
+
+// Enter a month (as a number between 1 and 12). Print the number of days in that month. Assume a non-leap year.
+//  - Check if a year is a leap year or not.
+
+let month = 3;
+let year = 7;
+
+if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+    console.log(`\n31 days in ${month} month`);
+} else if (month == 2) {
+    if (year % 4 == 0) {
+        console.log(`\n29 days in ${month} month`);
+    } else {
+        console.log(`\n28 days in ${month} month`);
+    }
+} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+    console.log(`\n30 days in ${month} month`);
 } else {
-    console.log(`\n\nInvalid Age: ${age}`);
+    console.log(`\n\nInvalid Month: ${month}`);
 }
