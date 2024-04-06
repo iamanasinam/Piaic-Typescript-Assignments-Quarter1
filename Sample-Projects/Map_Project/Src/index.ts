f12c4e5d561084d209165060a41e3e528d60d42f
Anas <iamanasinam@gmail.com>
2024-04-06 17:51:12 +0500

    google map simply added

--- Sample-Projects/Map_Project/Src/index.ts
+++ Sample-Projects/Map_Project/Src/index.ts
@@ -1,1 +1,18 @@
-console.log(`i am working`)
+import {User} from "./User";
+import { Company } from "./Company";
+
+// assiging from the User to the user
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0,
+    },
+  });
