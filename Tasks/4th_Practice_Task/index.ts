5ad7fc01ac66ba515a549c9eb548b83762ad5a5f
Anas <iamanasinam@gmail.com>
2024-05-03 13:28:32 +0500

    variable named properly assigned

--- Tasks/4th_Practice_Task/index.ts
+++ Tasks/4th_Practice_Task/index.ts
@@ -1,5 +1,5 @@
-//first function to be called
-const firstTask = (number: number) => {
+//first task
+const findNumberProperties = (number: number) => {
   console.log("Task 1\n");
   console.log(`the entered number is ${number}`);
   if (number % 2 === 0) {
@@ -25,8 +25,9 @@ const firstTask = (number: number) => {
   }
 };
 
-// second function
-const secondTask = (age: number, nationality: string) => {
+// second task
+
+const votingEligibility = (age: number, nationality: string) => {
   console.log("Task 2\n");
   console.log(`Age is ${age} and Nationality is ${nationality}`);
   if (age >= 18) {
@@ -40,8 +41,8 @@ const secondTask = (age: number, nationality: string) => {
   }
 };
 
-// third function
-const thirdFunction = (_age: number) => {
+// third task
+const ageGroup = (_age: number) => {
   console.log(`\nTask 3`);
   if (_age >= 0 && _age <= 12) {
     console.log(`\nChild: ${_age}`);
@@ -56,7 +57,7 @@ const thirdFunction = (_age: number) => {
   }
 };
 
-// fourth function
+// fourth task
 const monthNumber = (months: number) => {
   if (months === 1) {
     return `st`;
@@ -69,7 +70,7 @@ const monthNumber = (months: number) => {
   }
 };
 
-const fourthFunction = (month: number, year: number) => {
+const findingDaysOfMonth = (month: number, year: number) => {
   let status = monthNumber(month);
   console.log(`\nTask 4\n\nGiven month: ${month}${status}, year: ${year}`);
 
@@ -98,49 +99,49 @@ const fourthFunction = (month: number, year: number) => {
 };
 
 // fifth Task
-const fifthFuntion= (brithYear: number, yearNow: number)=>{
+const findingAge= (brithYear: number, yearNow: number)=>{
 let age :number = yearNow - brithYear;
 console.log(`Q1: Finding age based on the current year.\nI'm ${age} years old.`)}
 
 // sixth Task
-const sixthTask = (_length :number, _width :number)=>{
+const areaOfTriangle = (_length :number, _width :number)=>{
 let _areaOfTriangle: number = _length * _width;
 console.log(`\nQ2: Area of rectangle using length and width.\nLength = ${_length}, Width = ${_width}\nArea of Triangle = ${_areaOfTriangle}`)}
 
 // seventh Task
-const seventhTask= (_pie: number, _radius: number)=>{
+const areaOfCircle= (_pie: number, _radius: number)=>{
 let _areaOfCircle: number = _pie * (_radius**2);
 console.log(`\nQ3: Area of the circle.\nPie = ${_pie}, Radius = ${_radius}\nArea of Circle = ${_areaOfCircle}`)}
 
 // 8th task
-const eigthTask= (_s:number)=>{
+const areaOfCube= (_s:number)=>{
 let _areaOfCube: number = 6*(_s**2);
 console.log(`\nQ4: Area of the cube.\nS = ${_s}\nArea of Cube = 6 x ${_s} = ${_areaOfCube}`)}
 
 // 9th Task
-const ninethTask= (temperatureInFahrenheit:number)=>{
+const temperatureConversion= (temperatureInFahrenheit:number)=>{
 const temperatureInCelsius = (temperatureInFahrenheit - 32) * 5 / 9;
 console.log(`\nQ5: Temperature from Fahrenheit to Celsius and Vice versa \n${temperatureInFahrenheit} degrees Fahrenheit is equal to ${temperatureInCelsius.toFixed(2)} degrees Celsius.\n${temperatureInCelsius} degrees Celsius is equal to ${((temperatureInCelsius * 9 / 5) + 32).toFixed(2)} degrees Fahrenheit.`);}
 
 // 10th Task
-const tenthtask= (_seconds: number)=>{
+const sortingSeconds= (_seconds: number)=>{
 console.log(`\nQ6: Converting Sec into Min/Sec \n${_seconds} Seconds, Equal to ${(_seconds/60).toFixed(0)} Minutes, and ${_seconds%60} Seconds.`)}
 
 // 11th Task
-const eleventhtask= (totalMarks: number,obtainedMarks: number )=>{
+const calculatingMarksPercentage= (totalMarks: number,obtainedMarks: number )=>{
 let _percentage: number = (obtainedMarks/totalMarks)*100;
 console.log(`\nQ7: Calculating the percentage \nObtained Marks = ${obtainedMarks}, Total Marks = ${totalMarks}\nPercentage = ${_percentage}`)}
 
 
 
-// firstTask(7)
-// secondTask(9, "Pakistani");
-// thirdFunction(22);
-// fourthFunction(6, 2009);
-// fifthFuntion(2000, 2024);
-// sixthTask(26, 28);
-// seventhTask(3.14, 7)
-// eigthTask(7)
-// ninethTask(66)
-// tenthtask(2707)
-// eleventhtask(1100, 980)
+// findNumberProperties(7)
+// votingEligibility(9, "Pakistani");
+// ageGroup(22);
+// findingDaysOfMonth(6, 2009);
+// findingAge(2000, 2024);
+// areaOfTriangle(26, 28);
+// areaOfCircle(3.14, 7)
+// areaOfCube(7)
+// temperatureConversion(66)
+// sortingSeconds(2707)
+// calculatingMarksPercentage(1100, 980)
